1045a3043aefbd67fac1bd665898989e177c2f6e
cgx <cgx>
2016-12-13 00:01:43 +0800

    。。

--- src/com/android/launcher3/download/DownloadController.java
+++ src/com/android/launcher3/download/DownloadController.java
@@ -1,13 +1,12 @@
 package com.android.launcher3.download;
 
 import android.view.View;
-
 import com.android.launcher3.utils.PackageUtil;
 
 /**
  * Created by cgx on 2016/12/7.
+ * 控制下载和View的刷新, 下载载体是以单个View为最小单元的
  */
-
 public class DownloadController implements DownloadManager.DownloadObserver{
 
     private OnDownloadRefreshUI mTargView;
@@ -50,6 +49,7 @@ public class DownloadController implements DownloadManager.DownloadObserver{
         mDownloadManager.unRegisterObserver(this);
     }
 
+    //设置下载信息，如果该下载信息在下载任务中就刷新View
     public void setDwonloadInfo(DownloadInfo info){
         this.info = info;
         DownloadTaskInfo taskInfo = mDownloadManager.getDownloadMap().get(info.id);
@@ -58,6 +58,7 @@ public class DownloadController implements DownloadManager.DownloadObserver{
         }
     }
 
+    //下载按钮的点击执行
     public void executeClick(DownloadInfo info){
         if (mState == DownloadManager.STATE_NONE || mState == DownloadManager.STATE_PAUSED || mState == DownloadManager.STATE_ERROR) {
             mDownloadManager.download(info);
--- src/com/android/launcher3/download/OnDownloadRefreshUI.java
+++ src/com/android/launcher3/download/OnDownloadRefreshUI.java
@@ -2,6 +2,7 @@ package com.android.launcher3.download;
 
 /**
  * Created by cgx on 2016/12/8.
+ * 下载过程中View的刷新回调
  */
 
 public interface OnDownloadRefreshUI {
